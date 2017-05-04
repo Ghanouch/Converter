6ebd044e9907b84b946fdcab8b9b27752f978caf
IGhanouch <ghanouch.issam@gmail.com>
2017-05-04 18:28:16 +0100

    Mission Succes : WADL file to Object

--- src/WSDL/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.8-b130911.1802 
-// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2017.05.03 à 01:46:17 AM WEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.xmlsoap.org/wsdl/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.xmlsoap.schemas.wsdl;
--- src/XmlToObject_WSDL.java
+++ src/XmlToObject_WSDL.java
@@ -1,5 +1,44 @@
+import WADL.Application;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+
 /**
  * Created by l.IsSaM.l on 04/05/2017.
  */
 public class XmlToObject_WSDL {
+
+    public static String getPathFrom(String nameFile)
+    {
+        return "src\\ressources\\WSDL examples\\" + nameFile;
+    }
+
+    public static Application getObjectFromWADL(String nameFile)
+    {
+        try
+        {
+            // Pour igoner la  balise de name space xmlns
+            XMLInputFactory xif = XMLInputFactory.newFactory();
+            XMLStreamReader xsr = xif.createXMLStreamReader(new FileInputStream(getPathFrom(nameFile+".wadl")));
+            xsr = new Mapping_WSDL.XsiTypeReader(xsr);
+
+            // Unmarshall Xml --> Object
+            JAXBContext jc = JAXBContext.newInstance(Application.class);
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            Application app = (Application) unmarshaller.unmarshal(xsr);
+
+            return app;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+
 }
